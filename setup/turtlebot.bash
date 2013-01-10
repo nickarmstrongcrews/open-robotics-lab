431a36fb332752554d0ed6ca88432783b162cd71
mboulet <boulet@gmail.com>
2013-01-10 11:25:16 -0800

    updating turtlebot.bash for laptop install

--- setup/turtlebot.bash
+++ setup/turtlebot.bash
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+PROGNAME=$(basename $0)
+function error_exit
+{
+    echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+    exit 1
+}
+
+##############################################################################
+# Check for required files
+##############################################################################
+ENCASE="encase_0.0.1_i386.deb"
+[ ! -f "$ENCASE" ] && error_exit "Missing EnCase install"
+
 ##############################################################################
 # Install Essentials
 ##############################################################################
@@ -11,16 +24,12 @@ sudo sed -i -e 's/us\.archive\.ubuntu\.com/mirrors\.mit\.edu/' -e 's/security\.u
 sudo apt-get update && sudo apt-get dist-upgrade
  
 # Ubuntu Extras
-# Installs fonts and media, standard compilers, chrony for time sync, ssh for remote access, firewall, and ack
-sudo apt-get install -y ubuntu-restricted-extras build-essential keychain chrony openssh-server gufw ack
+# Installs fonts and media, standard compilers, chrony for time sync, ssh for remote access, and ack
+sudo apt-get install -y ubuntu-restricted-extras build-essential keychain chrony openssh-server ack
  
 # Install VCS
 ## All of the essential verison control tools
 sudo apt-get install -y git tig git-gui git-svn gitk subversion mercurial python-pip
- 
-# Xmonad
-## This is optional, I just like xmonad
-sudo apt-get install -y xmonad suckless-tools xmobar dwm dmenu trayer libghc6-xmonad-contrib-dev scrot cabal xclip
  
 # Editors
 ## Install vim and emacs, for those who don't like pico.
@@ -31,23 +40,126 @@ sudo apt-get install -y vim vim-gtk emacs
 sudo apt-get install -y rxvt-unicode tmux byobu terminator
  
 # Google Chrome
-cd /tmp && wget
-https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
- 
+pushd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_i386.deb
+popd
+
+# EnCase
+## Install the MIT Lincoln Laboratory EnCase Forensic Software
+sudo dpkg -i "$ENCASE"
+
+##############################################################################
+# Essential Configuration
+##############################################################################
+
+# Hostname
+## Make sure the hostname is turtlebot
+sudo bash -c 'echo "turtlebot" > /etc/hostname'
+
+# Dotfiles
+## Install Michael Carroll's dotfiles
+sudo pip install dotfiles
+mkdir -p ~/devel/lib/dotfiles
+git clone https://github.com/mjcarroll/dotfiles ~/devel/lib/dotfiles
+pushd ~/devel/lib/dotfiles/
+git branch -t iap origin/iap
+git checkout iap
+git submodule init && git submodule update
+dotfiles -s -R ~/devel/lib/dotfiles --force
+popd
+
+# Indicators
+## 
+mkdir -p ~/.config/autostart
+sudo apt-get install indicator-multiload indicator-cpufreq
+#TODO: add cpu, memory, and network graphs
+
+# Power
+## Do not suspend, hybernate, or shut down when the lid is closed
+# MANUAL: System Settings -> Power
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action 'nothing'
+
+# Host table
+## Set /etc/hosts
+sudo bash -c "cat >/etc/hosts" <<'EOF'
+127.0.0.1	localhost
+127.0.1.1	turtlebot
+
+192.168.1.200   turtlevm-static
+192.168.1.202   turtlebot-static
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
+
+# Network
+## Create Static-Address and DHCP-Address system connections
+sudo bash -c "cat >/etc/NetworkManager/system-connections/Static-Address" <<'EOF'
+[802-3-ethernet]
+duplex=full
+
+[connection]
+id=Static-Address
+uuid=UUID
+type=802-3-ethernet
+autoconnect=false
+
+[ipv6]
+method=auto
+
+[ipv4]
+method=manual
+addresses1=192.168.1.202;24;192.168.1.1;
+EOF
+UUID=`uuidgen`
+sudo sed -i /etc/NetworkManager/system-connections/Static-Address -e "s/UUID/$UUID/"
+sudo chmod 300 /etc/NetworkManager/system-connections/Static-Address 
+sudo bash -c "cat >/etc/NetworkManager/system-connections/DHCP-Address" <<'EOF'
+[802-3-ethernet]
+duplex=full
+
+[connection]
+id=DHCP-Address
+uuid=UUID
+type=802-3-ethernet
+
+[ipv6]
+method=auto
+
+[ipv4]
+method=auto
+EOF
+UUID=`uuidgen`
+sudo sed -i /etc/NetworkManager/system-connections/DHCP-Address -e "s/UUID/$UUID/"
+sudo chmod 300 /etc/NetworkManager/system-connections/DHCP-Address 
+
+# Launcher
+## Setup the Launcher
+gsettings set com.canonical.Unity.Launcher favorites ['nautilus-home.desktop', 'firefox.desktop', 'terminator.desktop']
+
 ##############################################################################
 # ROS
 ##############################################################################
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
+
+sudo apt-get install -y python-rosdep python-rosinstall python-rospkg
+sudo apt-get install -y ros-groovy-desktop-full
+sudo rosdep init; rosdep update
  
 ##############################################################################
 # Turtlebot-specific
 ##############################################################################
-# Latest build tools
-sudo apt-get install -y python-rosdep python-rosinstall python-rospkg
- 
+
+# Note: Much, but not all, of the below is redundant w/ ros-groovy-desktop-full.
+
 # Basic ROS
 sudo apt-get install -y ros-groovy-ros ros-groovy-ros-comm
  
@@ -83,3 +195,56 @@ sudo apt-get install -y libprotobuf-dev libprotoc-dev libtar-dev
 
 # Turtlebot and Viz
 sudo apt-get install -y ros-groovy-turtlebot* ros-groovy-rqt*
+
+##############################################################################
+# IAP
+##############################################################################
+
+# IAP Class Git Repo / open-robotics-lab workspace
+##
+mkdir -p ~/open-robotics-lab
+git clone https://github.com/mjcarroll/open-robotics-lab ~/open-robotics-lab
+pushd ~/open-robotics-lab
+git branch -t 1.0 origin/1.0
+git checkout 1.0
+rowws regenerate
+rosws merge /opt/ros/groovy
+rosws update
+popd
+
+# Setup .rosrc-local
+##
+bash -c "cat >~/.rosrc-local" <<'EOF'
+. ~/open-robotics-lab/setup.bash
+
+#export ROS_HOSTNAME=turtlebot-static
+#export ROS_MASTER_URI=http://turtlebot-static:11311/
+EOF
+
+# Setup iap_support
+##
+mkdir -p ~/devel/ros/iap_support
+bash -c "cat >~/devel/ros/iap_support/.rosinstall" <<'EOF'
+# IT IS UNLIKELY YOU WANT TO EDIT THIS FILE BY HAND,
+# UNLESS FOR REMOVING ENTRIES.
+# IF YOU WANT TO CHANGE THE ROS ENVIRONMENT VARIABLES
+# USE THE rosinstall TOOL INSTEAD.
+# IF YOU CHANGE IT, USE rosinstall FOR THE CHANGES TO TAKE EFFECT
+- git: { uri: 'https://github.com/Auburn-Automow/rososc.git', local-name: rososc, version: '0.2'}
+- git: { uri: 'https://github.com/Auburn-Automow/rososc_tutorials.git', local-name: rososc_tutorials, version: '0.2'}
+- git: { uri: 'https://github.com/Auburn-Automow/rososc_utilities.git', local-name: rososc_utilities, version: 'master'}
+- git: { uri: 'https://github.com/mjcarroll/yujin_ocs.git', local-name: yujin_ocs }
+EOF
+pushd ~/devel/ros/iap_support
+rowws regenerate
+rosws merge /opt/ros/groovy
+rosws update
+popd
+
+# Set .ssh folder
+##
+mkdir -p ~/.ssh
+cp ~/open-robotics-lab/setup/id_rsa.turtlebot ~/.ssh/id_rsa
+cp ~/open-robotics-lab/setup/id_rsa.pub.turtlebot ~/.ssh/id_rsa.pub
+cp ~/open-robotics-lab/setup/id_rsa.pub.turtlevm ~/.ssh/authorized_keys
+
